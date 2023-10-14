0d7c0dc8573e72b8fa97b493e4129691a9420fd8
Mela Maluenda <maluenda.mela@gmail.com>
2023-10-14 20:25:04 +0200

    Create the second test for the world class

--- public/World/World.test.js
+++ public/World/World.test.js
@@ -13,4 +13,16 @@ describe("Given the method intitWorld in the class Wolrd", () => {
       expect(values[0][0].isAlive).toBe(isExpectedValue);
     });
   });
+  describe("When it receives by argument the value 3 and false", () => {
+    test("then it should return two propierties pointing 7 and true", () => {
+      const expectedlength = 7;
+      const isExpectedValue = true;
+
+      const board = new World();
+      const values = board.initWorld(expectedlength, isExpectedValue);
+
+      expect(values.length).toBe(expectedlength);
+      expect(values[0][0].isAlive).toBe(isExpectedValue);
+    });
+  });
 });
